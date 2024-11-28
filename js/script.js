e1c50fdd5264003ebed5c071678427505ea37a3d
Elijah Porter <84803278+Codezilluh@users.noreply.github.com>
2024-11-27 20:51:50 -0500

    make random link work

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 addEventListener("DOMContentLoaded", () => {
 	const button = document.querySelector("#toggle");
+	const random = document.querySelector("#random");
 	const quickInfo = document.querySelector(".quickinfo");
+	const options = ["/index.html", "/most_viewed.html", "/most_edited.html"];
+
+	// Set the link href to a random article
+	random.href = options[Math.round(Math.random())];
 
 	button.addEventListener("click", () => {
 		button.classList.toggle("active");
